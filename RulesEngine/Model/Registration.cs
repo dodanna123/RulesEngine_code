7c409ee763e0e39f593b21349082823ca896aa34
jyotiranjannag <j.ranjan1979@gmail.com>
2020-08-07 05:41:16 -0700

    first commit rulesEngine

---
+++ RulesEngine/Model/Registration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RulesEngine.Rules;
+
+namespace RulesEngine.Model
+{
+    [RuleEngineType(RuleType = typeof(DefaultRuleEngine<Registration>))]
+    public class Registration
+    {
+        
+        [RequiredField("payment is for a physical product", "generate a packing slip for shipping")]
+        public string PaymentForPhysicalProduct { get; set; }
+
+        [RequiredField("payment is for a book","create a duplicate packing slip for the royalty department")]
+        public string PaymentForBook { get; set; }
+
+        [RequiredField("payment is for a membership", "activate that membership")]
+        public string PaymentForMembership { get; set; }
+
+        [RequiredField("payment is an upgrade to a membership"," apply the upgrade")]
+        public string PaymentForUpgradeToMembership { get; set;}
+
+        [RequiredField("payment is for a membership or upgrade"," e-mail the owner and inform them of the activation/upgrade")]
+        public string paymentForMembershipORUpgrade { get; set;}
+
+        [RequiredField("payment is for the video “Learning to Ski","add a free First Aid video to the packing slip (the result of a court decision in 1997)")]
+        public string paymentForThevideoLearningToSkip { get; set; }
+
+        [RequiredField("payment is for a physical product or a book"," generate a commission payment to the agent")]
+        public string paymentIsForPhysicalProductOrABook { get; set; }
+
+    }
+}
---
+++ RulesEngine/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RulesEngine.Model;
+using RulesEngine.Rules;
+
+namespace RulesEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+            Registration register = new Registration();
+
+            IRuleEngine<Registration> ruleEngine = RuleEngineFactory<Registration>.GetEngine();
+
+            //assign the value to  register.PaymentForPhysicalProduct else it will consider as the rule is broken and it will alert the error message.
+            //"PaymentForPhysicalProduct" is hardcoded. This value can be map from config file also
+            register.PaymentForPhysicalProduct = "PaymentForPhysicalProduct";
+
+            //assign the value to  register.PaymentForBook else it will consider as the rule is broken and it will alert the error message.
+            //"PaymentForBook" is hardcoded. This value can be map from config file also
+            register.PaymentForBook = "PaymentForBook";
+
+            //assign the value to  register.PaymentForMembership else it will consider as the rule is broken and it will alert the error message.
+            //"PaymentForMembership" is hardcoded. This value can be map from config file also
+            register.PaymentForMembership = "PaymentForMembership";
+
+            //assign the value to  register.PaymentForUpgradeToMembership else it will consider as the rule is broken and it will alert the error message.
+            //"PaymentForUpgradeToMembership" is hardcoded. This value can be map from config file also
+            register.PaymentForUpgradeToMembership = "PaymentForUpgradeToMembership";
+
+            //assign the value to  register.paymentForMembershipORUpgrade else it will consider as the rule is broken and it will alert the error message.
+            //"paymentForMembershipORUpgrade" is hardcoded. This value can be map from config file also
+            register.paymentForMembershipORUpgrade = "paymentForMembershipORUpgrade";
+
+            //assign the value to  register.paymentForThevideoLearningToSkip else it will consider as the rule is broken and it will alert the error message.
+            //"paymentForThevideoLearningToSkip" is hardcoded. This value can be map from config file also
+            register.paymentForThevideoLearningToSkip = "paymentForThevideoLearningToSkip";
+
+            //assign the value to  register.paymentIsForPhysicalProductOrABook else it will consider as the rule is broken and it will alert the error message.
+            //"paymentIsForPhysicalProductOrABook" is hardcoded. This value can be map from config file also
+            register.paymentIsForPhysicalProductOrABook = "paymentIsForPhysicalProductOrABook";
+
+            var results = ruleEngine.Validate(register);
+
+            foreach (var r in results)
+            {
+                if (r.IsBroken)
+                {
+                    Console.WriteLine("{0} rule is broken and the error is {1}", r.Name, r.ErrorMessage);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
